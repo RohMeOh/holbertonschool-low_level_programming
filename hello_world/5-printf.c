f2c35ae9688b3c31c10924d4be4afb5c1de63953
RohMeOh <romeogr21@gmail.com>
2026-02-10 14:15:28 -0400

    Update 4

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - prints a quote using puts
+ * main - prints a sentence using printf
  *
  * Return: Always 0
  */
 int main(void)
 {
-	printf(" with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
