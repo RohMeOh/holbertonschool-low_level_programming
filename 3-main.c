7b6dd78f30e736d5a2c30e514a16fac8fb10c31d
RohMeOh <romeogr21@gmail.com>
2026-03-14 06:23:10 -0400

    Updated

---
+++ 3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    int a, b;
+    int (*f)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    f = get_op_func(argv[2]);
+    if (f == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    if ((b == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    printf("%d\n", f(a, b));
+    return (0);
+}
