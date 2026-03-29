b46dbb625358466f1d174b873e4b76ff1e5f4e76
RohMeOh <romeogr21@gmail.com>
2026-03-29 05:48:13 -0400

    Updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,6 +18,35 @@ void close_fd(int fd)
 }
 
 /**
+ * print_error_97 - prints usage error
+ */
+void print_error_97(void)
+{
+	dprintf(2, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * print_error_98 - prints read error
+ * @file: file name
+ */
+void print_error_98(char *file)
+{
+	dprintf(2, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+/**
+ * print_error_99 - prints write error
+ * @file: file name
+ */
+void print_error_99(char *file)
+{
+	dprintf(2, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+/**
  * main - copies content from one file to another
  * @argc: number of arguments
  * @argv: argument vector
@@ -26,60 +55,37 @@ void close_fd(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd, wr, total, offset;
+	int fd_from, fd_to, rd, wr, total;
+
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		print_error_97();
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error_98(argv[1]);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close_fd(fd_from);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		print_error_99(argv[2]);
 
 	rd = read(fd_from, buffer, 1024);
 	while (rd > 0)
 	{
 		total = 0;
-		offset = 0;
-
 		while (total < rd)
 		{
-			wr = write(fd_to, buffer + offset, rd - total);
+			wr = write(fd_to, buffer + total, rd - total);
 			if (wr == -1)
-			{
-				close_fd(fd_from);
-				close_fd(fd_to);
-				dprintf(2, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
+				print_error_99(argv[2]);
 			total += wr;
-			offset += wr;
 		}
-
 		rd = read(fd_from, buffer, 1024);
 	}
 
 	if (rd == -1)
-	{
-		close_fd(fd_from);
-		close_fd(fd_to);
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error_98(argv[1]);
 
 	close_fd(fd_from);
 	close_fd(fd_to);
--- file_io/main.h
+++ file_io/main.h
@@ -7,5 +7,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_fd(int fd);
+void print_error_97(void);
+void print_error_98(char *file);
+void print_error_99(char *file);
 
 #endif
