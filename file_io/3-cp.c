8dd7ce5a2ee1c5cbd6cb51a590e1dd58ca87a366
RohMeOh <romeogr21@gmail.com>
2026-03-29 05:33:43 -0400

    Updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,9 +4,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * close_fd - closes a file descriptor
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
+	int fd_from, fd_to, rd, wr;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -25,42 +45,35 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close(fd_from);
+		close_fd(fd_from);
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
+	rd = 1024;
+	while (rd == 1024)
 	{
-		w = write(fd_to, buffer, r);
-		if (w == -1)
+		rd = read(fd_from, buffer, 1024);
+		if (rd == -1)
 		{
-			close(fd_from);
-			close(fd_to);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1)
+		{
+			close_fd(fd_from);
+			close_fd(fd_to);
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	if (r == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	close_fd(fd_from);
+	close_fd(fd_to);
 
 	return (0);
 }
